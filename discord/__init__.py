0ab9d3eaa12349f54d6631b444726030bb45481a
Tom <47765953+IAmTomahawkx@users.noreply.github.com>
2022-02-12 15:07:06 -0800

    add a discord_components check, raise an error if found (#158)
    * add a discord_components check, raise an error if found
    
    * add a discord_components check, raise an error if found

--- discord/__init__.py
+++ discord/__init__.py
@@ -72,3 +72,12 @@ class VersionInfo(NamedTuple):
 version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=0, releaselevel="alpha", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+try:
+    import discord_components
+    raise RuntimeError("package `discord_components` appears to be installed. This library is incompatible with Enhanced-discord.py, "
+                       "Please use the built in Views.\n"
+                       "Documentation can be found here: https://enhanced-dpy.readthedocs.io/en/latest/api.html#bot-ui-kit\n"
+                       "Examples can be found here: https://github.com/iDevision/enhanced-discord.py/tree/2.0/examples/views")
+except ImportError:
+    pass
